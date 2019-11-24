7317ca83790aef8737acd0e5074023f107501ae6
Marcin Maślach <marcin@MacBook-Air-Marcin.local>
2019-11-24 15:01:34 +0100

    add new templates for login and register page

--- Crowler/users/forms.py
+++ Crowler/users/forms.py
@@ -9,15 +9,15 @@ from Crowler.models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
-    password = PasswordField('Password',validators=[DataRequired()])
-    submit = SubmitField('Log In')
+    password = PasswordField('Hasło',validators=[DataRequired()])
+    submit = SubmitField('Zaloguj się!')
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
-    username = StringField('UserName',validators=[DataRequired()])
-    password = PasswordField('Password',validators=[DataRequired(),EqualTo('pass_confirm',message='Passwords must match!')])
-    pass_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
-    submit = SubmitField('Register!')
+    username = StringField('Nazwa użytkownika',validators=[DataRequired()])
+    password = PasswordField('Hasło',validators=[DataRequired(),EqualTo('pass_confirm',message='Passwords must match!')])
+    pass_confirm = PasswordField('Powtórz hasło',validators=[DataRequired()])
+    submit = SubmitField('Utwórz konto!')
 
     def check_email(self,field):
         if User.query.filter_by(email=field.data).first():
